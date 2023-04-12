960a65f755b05fcf12cc7fa622717b3ed14f80e8
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-12 15:04:43 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+
+st.title('PlantNet API')
+
+# Create a form for the API key
+api_key = st.text_input('Enter your PlantNet API key')
+
+# Create a form for the image file
+image_file = st.file_uploader('Upload an image')
+
+# Create a button to submit the form
+if st.button('Identify'):
+    # Make an API request
+    url = 'https://my-api.plantnet.org/v2/identify/all'
+    params = {
+        'include-related-images': 'false',
+        'no-reject': 'false',
+        'lang': 'en',
+        'api-key': 2b10PuYKEhDAUVIEKvdS6itjc
+    }
+    files = {
+        'images': image_file.read()
+    }
+    response = requests.post(url, params=params, files=files)
+
+    # Display the API results
+    if response.ok:
+        results = response.json()
+        st.json(results)
+    else:
+        st.error(f"API request failed with status code {response.status_code}")
