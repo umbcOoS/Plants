55ab92656153cbf7dda7450bfd1d878a7ef3dc80
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-13 09:48:42 -0400

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -1,56 +1,36 @@
 import streamlit as st
-from PIL import Image
-import base64
 import requests
 
-# Define Plant.id API endpoint
-api_url = "https://my.plantnet.org/api/identify/all?organs=auto"
-params = {
-        'include-related-images': 'true',
+st.title('PlantNet API')
+
+# Create a form for the image file
+image_file = st.file_uploader('Upload an image')
+
+# Create a button to submit the form
+if st.button('Identify'):
+    # Make an API request
+    url = 'https://my-api.plantnet.org/v2/identify/all'
+    params = {
+        'include-related-images': 'false',
         'no-reject': 'false',
         'lang': 'en',
         'api-key': '2b10PuYKEhDAUVIEKvdS6itjc'
     }
-
-# Define function to send image to Plant.id API
-def send_image(image):
-    headers = {
-        'Content-Type': 'application/octet-stream'
+    files = {
+        'images': image_file.read()
     }
-    response = requests.post(api_url, headers=headers, data=image)
-    return response.json()
+    response = requests.post(url, params=params, files=files)
 
-# Streamlit app
-def main():
-    st.title("Plant.id Web Component Example")
-    st.write("Upload an image of a plant and get the identification result using the Plant.id API.")
-
-    # Upload image file
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-
-    # Process uploaded image
-    if uploaded_file is not None:
-        # Convert image to bytes
-        image_bytes = uploaded_file.read()
-        image = Image.open(uploaded_file)
-
-        # Display uploaded image
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-
-        # Convert image to base64 string
-        image_base64 = base64.b64encode(image_bytes).decode("utf-8")
-
-        # Call Plant.id API and display results
-        response = send_image(image_bytes)
-        if "results" in response:
-            st.subheader("Identification Results:")
-            for result in response["results"]:
-                st.write("Genus:", result["genus"])
-                st.write("Species:", result["species"])
-                st.write("Score:", result["score"])
-                st.write("---")
-        else:
-            st.error("Failed to get identification results")
-
-if __name__ == "__main__":
-    main()
+    # Display the API results
+    if response.ok:
+        results = response.json()
+        # Display the images
+        for result in results['suggestions']:
+            image_url = result['plant_name']['complete_image']['url']
+            st.image(image_url, caption=result['plant_name']['common_names']['en'], width=300)
+            # Display the stylized text
+            st.markdown(f"**Common Name:** {result['plant_name']['common_names']['en']}")
+            st.markdown(f"**Family:** {result['plant_name']['family']['scientific_name']['genus']} {result['plant_name']['family']['scientific_name']['species']}")
+            st.markdown(f"**Genus:** {result['plant_name']['genus']['scientific_name']['genus']} {result['plant_name']['genus']['scientific_name']['species']}")
+    else:
+        st.error(f"API request failed with status code {response.status_code}")
