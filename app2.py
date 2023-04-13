f065e44ff92903adcae6ade2763e804387f3c60c
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-13 11:14:56 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,13 +1,36 @@
 import streamlit as st
+import requests
+from retool import Retool
 
-# Define the Streamlit app
-def app():
-    st.title('UMBC Invasive Species ID')
+# Create a Retool instance
+retool = Retool('https://umbcsustainability.retool.com/apps/45db685c-d956-11ed-b982-cf060df0d768/Plant') # Replace with your Retool app URL and API key
 
-    # Embed the web component using an iframe
-    st.markdown('<iframe src="https://plantnet.org/ai-taxonomist-demo/" '
-                'style="width:400%; height:700px;"></iframe>',
-                unsafe_allow_html=True)
+st.title('UMBC Invasives ID')
 
-if __name__ == '__main__':
-    app()
+# Create a form for the image file
+image_file = st.file_uploader('Upload an image')
+
+# Create a button to submit the form
+if st.button('Identify'):
+    # Make an API request
+    url = 'https://my-api.plantnet.org/v2/identify/all'
+    params = {
+        'include-related-images': 'false',
+        'no-reject': 'false',
+        'lang': 'en',
+        'api-key': '2b10PuYKEhDAUVIEKvdS6itjc'
+    }
+    files = {
+        'images': image_file.read()
+    }
+    response = requests.post(url, params=params, files=files)
+
+    # Display the API results
+    if response.ok:
+        results = response.json()
+        st.json(results)
+        
+        # Send results to Retool
+        retool.send_data('plantnet_results', results) # Replace 'plantnet_results' with the name of the Retool query or variable you want to update
+    else:
+        st.error(f"API request failed with status code {response.status_code}")
