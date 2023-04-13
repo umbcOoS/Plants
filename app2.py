362188d9ce2ca1217a6f6ad5cc283774b7acce52
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-13 13:41:10 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import requests
+from PIL import Image
+from io import BytesIO
 
 st.title('UMBC Invasives IDbeta')
-
 
 # Create a form for the image file
 image_file = st.file_uploader('Upload an image')
@@ -27,16 +28,22 @@ if st.button('Identify'):
         results = response.json()
         for result in results['results']:
             st.markdown('**Result**')
-            st.write('Common name:', result['plant_name']['common_name'])
-            st.write('Scientific name:', result['plant_name']['scientific_name'])
-            image_url = result['images'][0]['url']
-            image_response = requests.get(image_url)
-            image = Image.open(BytesIO(image_response.content))
-            st.image(image, caption='Plant Image', use_column_width=True)
+            if 'plant_name' in result:
+                st.write('Common name:', result['plant_name'].get('common_name', 'N/A'))
+                st.write('Scientific name:', result['plant_name'].get('scientific_name', 'N/A'))
+                st.write('Family:', result['plant_name'].get('family', 'N/A'))
+                st.write('Genus:', result['plant_name'].get('genus', 'N/A'))
+                st.write('Species:', result['plant_name'].get('species', 'N/A'))
+            else:
+                st.write('Plant name information not available')
+            if 'images' in result:
+                image_url = result['images'][0]['url']
+                image_response = requests.get(image_url)
+                image = Image.open(BytesIO(image_response.content))
+                st.image(image, caption='Plant Image', use_column_width=True)
+            else:
+                st.write('Plant image not available')
             st.write('Confidence:', result['score'])
-            st.write('Family:', result['plant_name']['family'])
-            st.write('Genus:', result['plant_name']['genus'])
-            st.write('Species:', result['plant_name']['species'])
             st.write('Plant ID:', result['plant_id'])
             st.markdown('---')
     else:
