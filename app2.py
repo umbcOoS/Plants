b5c02ad6c37eea6335caf822065ee9be717c2a48
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-13 13:27:17 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import requests
-from retool import Retool
-
-# Create a Retool instance
-retool = Retool('https://umbcsustainability.retool.com/apps/45db685c-d956-11ed-b982-cf060df0d768/Plant') # Replace with your Retool app URL and API key
 
 st.title('UMBC Invasives ID')
 
@@ -15,7 +11,7 @@ if st.button('Identify'):
     # Make an API request
     url = 'https://my-api.plantnet.org/v2/identify/all'
     params = {
-        'include-related-images': 'false',
+        'include-related-images': 'true',
         'no-reject': 'false',
         'lang': 'en',
         'api-key': '2b10PuYKEhDAUVIEKvdS6itjc'
@@ -28,9 +24,8 @@ if st.button('Identify'):
     # Display the API results
     if response.ok:
         results = response.json()
-        st.json(results)
-        
-        # Send results to Retool
-        retool.send_data('plantnet_results', results) # Replace 'plantnet_results' with the name of the Retool query or variable you want to update
+        st.markdown('**API Results**')
+        st.json(results, indent=4, expand_all=True)
     else:
         st.error(f"API request failed with status code {response.status_code}")
+st.json(results, indent=4, expand_all=True, css='font-size: 16px; line-height: 1.5;')
