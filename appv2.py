bd4393f08286ed5f8a64b174fcc4c8ea8bc6fb97
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-13 09:39:05 -0400

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import requests
+from PIL import Image
+import base64
+import requests
+
 
 # Define the HTML and CSS code
 html_template = '''
@@ -47,3 +51,37 @@ if st.button('Identify'):
         st.json(results)
     else:
         st.error(f"API request failed with status code {response.status_code}")
+# Streamlit app
+def main():
+    st.title("Plant.id Web Component Example")
+    st.write("Upload an image of a plant and get the identification result using the Plant.id API.")
+
+    # Upload image file
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    # Process uploaded image
+    if uploaded_file is not None:
+        # Convert image to bytes
+        image_bytes = uploaded_file.read()
+        image = Image.open(uploaded_file)
+
+        # Display uploaded image
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        # Convert image to base64 string
+        image_base64 = base64.b64encode(image_bytes).decode("utf-8")
+
+        # Call Plant.id API and display results
+        response = send_image(image_bytes)
+        if "results" in response:
+            st.subheader("Identification Results:")
+            for result in response["results"]:
+                st.write("Genus:", result["genus"])
+                st.write("Species:", result["species"])
+                st.write("Score:", result["score"])
+                st.write("---")
+        else:
+            st.error("Failed to get identification results")
+
+if __name__ == "__main__":
+    main()
