4e509a12b0ecabe50f2b4899b9e295ac6c0bdce3
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-13 10:54:26 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 # Define the Streamlit app
 def app():
-    st.title('PlantNet AI Taxonomist Demo')
+    st.title('UMBC Invasive Species ID')
 
     # Embed the web component using an iframe
     st.markdown('<iframe src="https://plantnet.org/ai-taxonomist-demo/" '
-                'style="width:100%; height:500px;"></iframe>',
+                'style="width:400%; height:700px;"></iframe>',
                 unsafe_allow_html=True)
 
 if __name__ == '__main__':
