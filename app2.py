d9a0969d4e853d11a135f085e86ae7cf32bba2b5
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-14 07:40:45 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -38,7 +38,7 @@ if response.ok:
 
         st.markdown('**Result**')
         score_percent = result['score'] * 100  # Convert score to percentage
-        st.markdown(f'<p style="font-size:24px;font-weight:bold;">Confidence: {score_percent:.2f}%</p>', unsafe_allow_html=True)  # Display score as a percentage with 2 decimal places, in bold and larger font
+        st.markdown(f'<p style="font-size:24px;font-weight:bold;">ID Confidence: {score_percent:.2f}%</p>', unsafe_allow_html=True)  # Display score as a percentage with 2 decimal places, in bold and larger font
         st.write('Common name:', result['species']['commonNames'][0])
         st.write('Scientific name:', result['species']['scientificNameWithoutAuthor'])
         st.markdown('*Scientific name:*', unsafe_allow_html=True)  # Italicize "Scientific name" label
