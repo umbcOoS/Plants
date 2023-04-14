b06c5981d435f55804ba075c9ec767a18074b2bd
umbcOoS <130588878+umbcOoS@users.noreply.github.com>
2023-04-14 07:37:43 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -38,9 +38,12 @@ if response.ok:
 
         st.markdown('**Result**')
         score_percent = result['score'] * 100  # Convert score to percentage
+        st.markdown(f'<p style="font-size:24px;font-weight:bold;">Confidence: {score_percent:.2f}%</p>', unsafe_allow_html=True)  # Display score as a percentage with 2 decimal places, in bold and larger font
         st.write('Confidence:', f'{score_percent:.1f}%')  # Display score as a percentage with 1 decimal places
         st.write('Common name:', result['species']['commonNames'][0])
         st.write('Scientific name:', result['species']['scientificNameWithoutAuthor'])
+        st.markdown('*Scientific name:*', unsafe_allow_html=True)  # Italicize "Scientific name" label
+        st.markdown(f'*{result["species"]["scientificNameWithoutAuthor"]}*')  # Italicize scientific name result
         gbif_id = result['gbif']['id']
         gbif_url = f'https://www.gbif.org/species/{gbif_id}'
         st.markdown(f'GBIF: [{gbif_id}]({gbif_url})', unsafe_allow_html=True)
